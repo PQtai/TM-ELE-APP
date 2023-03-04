ff2022a6f86fc5dc12b5e157a45aaa43956ace83
PQTai <phamquoctai09082001@gmail.com>
2023-03-04 20:28:03 +0700

    Config routes,install css library,create sidebar and navbar for the admin layout

--- client/src/routes/routes.ts
+++ client/src/routes/routes.ts
@@ -1,4 +1,5 @@
 import config from '~/config';
+import overview from '~/pages/admin/overview/overview';
 import PostManager from '~/pages/admin/postManager/postManager';
 import Home from '~/pages/client/Home';
 import { IChildrenComponentProps, IRoutes } from '~/types/models/global';
@@ -9,7 +10,7 @@ const publicRoutes: IRoutes[] = [{ path: config.routes.home, component: Home }];
 const privateRoutesUser: IRoutes[] = [{ path: config.routes.home, component: Home }];
 
 const privateRoutesAdmin: IRoutes[] = [
-   { path: config.routes.homeAdmin, component: Home },
+   { path: config.routes.homeAdmin, component: overview },
    { path: config.routes.postManager, component: PostManager },
 ];
 
