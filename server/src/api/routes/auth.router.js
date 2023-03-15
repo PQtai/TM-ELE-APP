d120771a1636c7482364f39f920775452cd8b43a
PQTai <phamquoctai09082001@gmail.com>
2023-03-15 22:49:20 +0700

    Handle the auto-login function when a user registers an account

--- server/src/api/routes/auth.router.js
+++ server/src/api/routes/auth.router.js
@@ -1,70 +1,79 @@
 import express from 'express';
-import userControllers from '../controllers/auth.controller.js'
-import authMiddleware from '../middlewares/auth.middleware.js'
-import userValidation from '../helpers/userValidation.js'
+import userControllers from '../controllers/auth.controller.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
+import userValidation from '../helpers/userValidation.js';
 import validateMiddleware from '../middlewares/validate.middleware.js';
-import passport from "passport";
+import passport from 'passport';
 import '../middlewares/passport.js';
 const router = express.Router();
 
 // Route add user
 router.post(
-    '/register', 
-    validateMiddleware('body', userValidation), 
-    authMiddleware.checkRole, userControllers.registerAccount);
+  '/register',
+  authMiddleware.checkRole,
+  validateMiddleware('body', userValidation),
+  userControllers.registerAccount
+);
 
 // Route register for Admin
 router.post(
-    '/admin-register',
-    authMiddleware.authIsAdmin,
-    validateMiddleware('body', userValidation),
-    userControllers.registerAccount)
+  '/admin-register',
+  authMiddleware.authIsAdmin,
+  validateMiddleware('body', userValidation),
+  userControllers.registerAccount
+);
 
-// Route Login 
+// Route Login
 router.post(
-    '/login', 
-    validateMiddleware('body', userValidation), userControllers.login);
+  '/login',
+  validateMiddleware('body', userValidation),
+  userControllers.login
+);
 
 // Route Refresh
-router.post(
-    '/refresh', 
-    userControllers.requestRefreshToken);
+router.post('/refresh', userControllers.requestRefreshToken);
 
 // Route get user/id
 router.get(
-    '/get-user/:id', 
-    authMiddleware.verifyToken , userControllers.getUserById);
+  '/get-user/:id',
+  authMiddleware.verifyToken,
+  userControllers.getUserById
+);
 
 // Route user all
-router.get(
-    '/get-all', 
-    authMiddleware.authIsAdmin, userControllers.getAllUsers);
+router.get('/get-all', authMiddleware.authIsAdmin, userControllers.getAllUsers);
 
 // Route lock user
-router.patch(
-    '/:id/lock', 
-    authMiddleware.authIsAdmin, userControllers.lockUser);
+router.patch('/:id/lock', authMiddleware.authIsAdmin, userControllers.lockUser);
 
 // Route edit user
 router.patch(
-    '/edit/:id', 
-    authMiddleware.authIsAdminOrIsAuthor, userControllers.editUser);
+  '/edit/:id',
+  authMiddleware.authIsAdminOrIsAuthor,
+  userControllers.editUser
+);
 
 // Route change password
 router.post(
-    '/change-password', 
-    authMiddleware.authIsAdminOrIsAuthor, userControllers.changePassword);
+  '/change-password',
+  authMiddleware.authIsAdminOrIsAuthor,
+  userControllers.changePassword
+);
 
 // Route forgot password
 router.post(
-    '/forgot-password', 
-    authMiddleware.authIsAdminOrIsAuthor, userControllers.forgotPassword);
+  '/forgot-password',
+  authMiddleware.authIsAdminOrIsAuthor,
+  userControllers.forgotPassword
+);
 
 // Route logout
-router.post(
-    '/logout', 
-    authMiddleware.verifyToken, userControllers.logout);
+router.post('/logout', authMiddleware.verifyToken, userControllers.logout);
 
-router.get('/secret', passport.authenticate('jwt', { session: false }), userControllers.getAllUsers)
+router.get(
+  '/secret',
+  passport.authenticate('jwt', { session: false }),
+  userControllers.getAllUsers
+);
 
 export default router;
