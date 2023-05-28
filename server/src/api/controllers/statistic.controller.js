9f6dd0e6a7512252e7b83b19199c0c2d88a99781
minhhieu04 <minhhieu.tran.mcs@gmail.com>
2023-05-28 13:32:03 +0700

    Fix bug to calculate subscription percentage. Handle data response when query?= start_date && end_date || date

--- server/src/api/controllers/statistic.controller.js
+++ server/src/api/controllers/statistic.controller.js
@@ -5,117 +5,141 @@ import errorFunction from "../utils/errorFunction.js";
 const statistic = {
   userStatistics: async (req, res) => {
     try {
-      // Lấy ngày, tuần, tháng, quý hiện tại
-      const today = moment().startOf("day");
-      const thisWeek = moment().startOf("week");
-      const thisMonth = moment().startOf("month");
-      const thisQuarter = moment().startOf("quarter");
-
-      const { start_date, end_date } = req.query;
+      const { start_date, end_date, date } = req.query;
       let query = {};
+      let result = {};
+      // Nếu query truyền vào là start_date && end_date
       if (start_date && end_date) {
-        query.createdAt = { $gte: moment(start_date), $lte: moment(end_date) };
-      }
+        console.log(start_date);
+        const startDate = moment(start_date).startOf("day");
+        const endDate = moment(end_date).endOf("day");
+        query.createdAt = { $gte: startDate, $lte: endDate };
+        const totalUsers = await User.countDocuments(query);
+        result = { totalUsers };
+        // return res.status(200).json(errorFunction(false, 200, {totalUsers}))
+      } else {
+        // Kiểm tra nếu query `date` không được truyền vào, gán giá trị mặc định là ngày hôm nay
+        const dateQuery = date
+          ? moment(date).startOf("day")
+          : moment().startOf("day");
 
-      // Đếm số lượng user register trong ngày, tuần, tháng, quý
-      const totalUsers = await User.countDocuments(query);
-      const registeredToday = await User.countDocuments({
-        createdAt: { $gte: today },
-      });
-      const registeredThisWeek = await User.countDocuments({
-        createdAt: { $gte: thisWeek },
-      });
-      const registeredThisMonth = await User.countDocuments({
-        createdAt: { $gte: thisMonth },
-      });
-      const registeredThisQuarter = await User.countDocuments({
-        createdAt: { $gte: thisQuarter },
-      });
+        // Lấy ngày, tuần, tháng, quý của ngày nằm trong dataQuery
+        const thisWeekOfDate = moment(dateQuery).startOf("week");
+        const thisMonthOfDate = moment(dateQuery).startOf("month");
+        const thisQuarterOfDate = moment(dateQuery).startOf("quarter");
+        // Tính tổng user có trong hệ thống đã có trước ngày nằm trong dataquery
+        const totalUsers = await User.countDocuments({
+          createdAt: { $lte: dateQuery.endOf("day") },
+        });
+        // Đếm số lượng user register trong ngày, tuần, tháng, quý kể từ ngày nằm trong dataquery
+        const registeredOnDate = await User.countDocuments({
+          createdAt: { $gte: dateQuery, $lte: dateQuery.endOf("day") },
+        });
+        const registeredThisWeekOfDate = await User.countDocuments({
+          createdAt: { $gte: thisWeekOfDate },
+        });
+        const registeredThisMonthOfDate = await User.countDocuments({
+          createdAt: { $gte: thisMonthOfDate },
+        });
+        const registeredThisQuarterOfDate = await User.countDocuments({
+          createdAt: { $gte: thisQuarterOfDate },
+        });
+        // Đếm số lượng user register trong ngày, tuần, tháng, quý trước (tính từ ngày nằm trong query)
+        const lastDay = moment(dateQuery).subtract(1, "days");
+        const lastWeek = moment(thisWeekOfDate).subtract(1, "weeks");
+        const lastMonth = moment(thisMonthOfDate).subtract(1, "months");
+        const lastQuarter = moment(thisQuarterOfDate).subtract(1, "quarters");
 
-      // Lấy số lượng user đã đăng ký trong ngày, tuần, tháng, quý trước
-      const lastDay = moment(today).subtract(1, "days");
-      const lastWeek = moment(thisWeek).subtract(1, "weeks");
-      const lastMonth = moment(thisMonth).subtract(1, "months");
-      const lastQuarter = moment(thisQuarter).subtract(1, "quarters");
+        // .... Chức năng hiển thị danh sách users dựa vào các bộ đếm sẽ tạm thời được ẩn
+        // .... filter hoặc API riêng, khi người dùng click vào mới thực hiện render ra danh sách
+        // const userRegisteredBeforeDate = await User.find({
+        //   createdAt: { $lte: dateQuery.endOf("day") },
+        // });
+        // const userRegisteredOnDate = await User.find({
+        //   createdAt: { $gte: dateQuery, $lte: dateQuery.endOf("day") },
+        // });
+        // const usersRegisteredLastDay = await User.find({
+        //   createdAt: { $gte: lastDay, $lt: dateQuery },
+        // });
+        // console.log(usersRegisteredLastDay);
+        // const usersRegisteredLastWeek = await User.find({
+        //   createdAt: { $gte: lastWeek, $lt: dateQuery },
+        // });
+        // const usersRegisteredLastMonth = await User.find({
+        //   createdAt: { $gte: lastMonth, $lt: dateQuery },
+        // });
+        // const usersRegisteredLastQuarter = await User.find({
+        //   createdAt: { $gte: lastQuarter, $lt: dateQuery },
+        // });
 
-      const registeredLastDay = await User.countDocuments({
-        createdAt: { $gte: lastDay, $lt: today },
-      });
-      const registeredLastWeek = await User.countDocuments({
-        createdAt: { $gte: lastWeek, $lt: thisWeek },
-      });
-      const registeredLastMonth = await User.countDocuments({
-        createdAt: { $gte: lastMonth, $lt: thisMonth },
-      });
-      const registeredLastQuarter = await User.countDocuments({
-        createdAt: { $gte: lastQuarter, $lt: thisQuarter },
-      });
+        const registeredLastDay = await User.countDocuments({
+          createdAt: { $gte: lastDay, $lt: dateQuery },
+        });
+        const registeredLastWeek = await User.countDocuments({
+          createdAt: { $gte: lastWeek, $lt: thisWeekOfDate },
+        });
+        const registeredLastMonth = await User.countDocuments({
+          createdAt: { $gte: lastMonth, $lt: thisMonthOfDate },
+        });
+        const registeredLastQuarter = await User.countDocuments({
+          createdAt: { $gte: lastQuarter, $lt: thisQuarterOfDate },
+        });
 
-      // Mức độ tăng trưởng tính theo tỉ lệ
-      //   const registeredTodayRate =
-      //     registeredToday && registeredLastDay
-      //       ? registeredToday / registeredLastDay
-      //       : 0;
-      //   const registeredThisWeekRate =
-      //     registeredThisWeek && registeredLastWeek
-      //       ? registeredThisWeek / registeredLastWeek
-      //       : 0;
-      //   const registeredThisMonthRate =
-      //     registeredThisMonth && registeredLastMonth
-      //       ? registeredThisMonth / registeredLastMonth
-      //       : 0;
-      //   const registeredThisQuarterRate =
-      //     registeredThisQuarter && registeredLastMonth
-      //       ? registeredThisQuarter / registeredLastQuarter
-      //       : 0;
-
-      // Tính tỉ lệ người dùng đăng ký tăng/ giảm tính theo phần trăm
-      const registeredTodayRate =
-        registeredLastDay === 0
-          ? 100
-          : (
-              ((registeredToday - registeredLastDay) / registeredLastDay) *
+        // Tính tỉ lệ người dùng đăng ký tăng/ giảm tính theo phần trăm
+        const registeredThisDateRate = (
+          registeredLastDay === 0
+            ? registeredOnDate === 0
+              ? 0
+              : Infinity
+            : ((registeredOnDate - registeredLastDay) /
+                Math.abs(registeredLastDay)) *
               100
-            ).toFixed(2) || 0;
-      const registeredThisWeekRate =
-        registeredLastWeek === 0
-          ? 100
-          : (
-              ((registeredThisWeek - registeredLastWeek) / registeredLastWeek) *
+        ).toFixed(2);
+        const registeredThisWeekRate = (
+          registeredLastWeek === 0
+            ? registeredThisWeekOfDate === 0
+              ? 0
+              : Infinity
+            : ((registeredThisWeekOfDate - registeredLastWeek) /
+                Math.abs(registeredLastWeek)) *
               100
-            ).toFixed(2) || 0;
-      const registeredThisMonthRate =
-        registeredLastMonth === 0
-          ? 100
-          : (
-              ((registeredThisMonth - registeredLastMonth) /
-                registeredLastMonth) *
+        ).toFixed(2);
+        const registeredThisMonthRate = (
+          registeredLastMonth === 0
+            ? registeredThisMonthOfDate === 0
+              ? 0
+              : Infinity
+            : ((registeredThisMonthOfDate - registeredLastMonth) /
+                Math.abs(registeredLastMonth)) *
               100
-            ).toFixed(2) || 0;
-      const registeredThisQuarterRate =
-        registeredLastQuarter === 0
-          ? 0
-          : (
-              ((registeredThisQuarter - registeredLastQuarter) /
-                registeredLastQuarter) *
+        ).toFixed(2);
+        const registeredThisQuarterRate = (
+          registeredLastQuarter === 0
+            ? registeredThisQuarterOfDate === 0
+              ? 0
+              : Infinity
+            : ((registeredThisQuarterOfDate - registeredLastQuarter) /
+                Math.abs(registeredLastQuarter)) *
               100
-            ).toFixed(2) || 0;
+        ).toFixed(2);
 
-      return res.status(201).json(
-        errorFunction("false", 201, {
+        result = {
           totalUsers,
-          registeredToday,
-          registeredThisWeek,
-          registeredThisMonth,
-          registeredTodayRate: `${registeredTodayRate}%`,
+          registeredOnDate,
+          registeredThisWeekOfDate,
+          registeredThisMonthOfDate,
+          registeredThisQuarterOfDate,
+          registeredThisDateRate: `${registeredThisDateRate}%`,
           registeredThisWeekRate: `${registeredThisWeekRate}%`,
           registeredThisMonthRate: `${registeredThisMonthRate}%`,
           registeredThisQuarterRate: `${registeredThisQuarterRate}%`,
-        })
-      );
+        };
+      }
+
+      return res.status(200).json(errorFunction(false, 200, result));
     } catch (err) {
       console.log(err);
-      res.status(500).json(errorFunction(true, 500, err.message));
+      res.status(500).json(errorFunction(true, 500, "Internal server error"));
     }
   },
   postStatistics: async (req, res) => {
